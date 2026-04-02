a9fc86cbaad166db942f79c31c159172d97805d7
Ojas Solanki <126442921+Ojas-xd@users.noreply.github.com>
2026-04-02 23:10:50 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+# Use DATABASE_URL from environment
+engine = create_engine(settings.DATABASE_URL)  # no connect_args for Postgres
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
